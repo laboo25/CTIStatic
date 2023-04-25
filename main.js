d1ca3898205ddd55a90ba21c195244773e09d7bc
laboo25 <89362256+laboo25@users.noreply.github.com>
2023-04-25 20:37:25 +0600

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ imgs[4] = 'https://i.postimg.cc/c4XMFhxd/Bg-4.jpg';
 imgs[5] = 'https://i.postimg.cc/G2FvGJgw/Bg-5.jpg';
 imgs[6] = 'https://i.postimg.cc/CM3Dx2cP/Bg-6.jpg';
 imgs[7] = 'https://i.postimg.cc/pVhzxvJ5/Bg-7.jpg';
+imgs[8] = 'https://i.postimg.cc/d3syf8Yp/Bg-8.jpg';
+
 window.onload = function () {
     const random = Math.floor(Math.random() * imgs.length);
     document.body.style.backgroundImage = `url(${imgs[random]})`;
