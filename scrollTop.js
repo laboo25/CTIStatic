4a569eccdf5d5597ceb2d2cad794576c21de21fa
lab00 <89362256+laboo25@users.noreply.github.com>
2023-11-11 21:25:02 +0600

    Update scrollTop.js

--- scrollTop.js
+++ scrollTop.js
@@ -1,25 +1,7 @@
-var scrollToTopBtn = document.createElement("button.btn");
-        scrollToTopBtn.innerHTML = "Scroll to Top";
-        scrollToTopBtn.id = "scrollToTopBtn";
-
-        // Append the button to the body
-        document.body.appendChild(scrollToTopBtn);
-
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function() {
-            scrollFunction();
-        };
-
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                scrollToTopBtn.style.display = "block";
-            } else {
-                scrollToTopBtn.style.display = "none";
-            }
-        }
-
-        // When the user clicks on the button, scroll to the top of the document
-        scrollToTopBtn.addEventListener("click", function() {
-            document.body.scrollTop = 0; // For Safari
-            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-        });
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
